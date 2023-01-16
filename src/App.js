d9b469b40ca01cc1ee1bc43c33a7da4b6f9f2711
Ehtisham <ehtishamashraf91@gmail.com>
2023-01-16 17:21:56 +0500

    creating basic structure and integrating mui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./pages/Home";
+import Dashboard from "./components/dashboard";
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Add Customers</div>;
+};
+
+export default Home;
